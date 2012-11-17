1f89b21e902bdb1911adf58a8f23e837f58ccfdb
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-17 03:20:37 -0500

    Merge get-file into get-dictionary

--- scrabble.lisp
+++ scrabble.lisp
@@ -1,12 +1,9 @@
-(defun get-file (filename)
+(defun get-dictionary ()
+	(setf filename "dictionary.txt")
 	(with-open-file (stream filename)
 		(loop for line = (read-line stream nil)
 			while line
 			collect line)))
-
-(defun get-dictionary ()
-	(setf filename "dictionary.txt")
-	(get-file filename))
 
 (defun print-list (lst)
 	(loop for item in lst do
