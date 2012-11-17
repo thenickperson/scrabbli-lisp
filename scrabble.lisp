3d1d21274ce7423ff6379fba9ec086306e0e0c63
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-17 01:30:34 -0500

    Delete unneeded comments and whitespace

--- scrabble.lisp
+++ scrabble.lisp
@@ -8,20 +8,9 @@
 	(setf filename "dictionary.txt")
 	(get-file filename))
 
-#|
-;; this would work if our program was compiled, not interpretted AND
-;; if the compiler included a tail-recursive optimizer
-(defun print-list (lst)
-	(when (first lst)
-	  (format t "~a~%" (first lst))
-	  (print-list (rest lst))))
-|#
-
-;;instead, we have to do this
 (defun print-list (lst)
   (loop for item in lst do
 	(format t "~a~%" i)))
-
 
 (defun main ()
 	(print-list (get-dictionary)))
