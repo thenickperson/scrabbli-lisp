fff827ecc7065329ed4c88721343fad20115494f
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-17 04:59:29 -0500

    Add get-letter-score and string-in-list functions (also fixes get-word-score)

--- scrabbli.lisp
+++ scrabbli.lisp
@@ -9,8 +9,23 @@
 	(loop for item in lst do
 		(format t "~a~%" item)))
 
+(defun string-in-list (str lst)
+	(setf found nil)
+	(loop for item in lst do
+		(if (string= str item) (setf found t)))
+	found)
+
+(defun get-letter-score (letter)
+	(cond
+		((string-in-list letter '("E" "A" "I" "O" "N" "R" "T" "L" "S" "U")) 1)
+		((string-in-list letter '("D" "G"))                                 2)
+		((string-in-list letter '("B" "C" "M" "P"))                         3)
+		((string-in-list letter '("F" "H" "V" "W" "Y"))                     4)
+		((string-in-list letter '("K"))                                     5)
+		((string-in-list letter '("J" "X"))                                 8)
+		((string-in-list letter '("Q" "Z"))                                 10)
+		(t 0)))
+
 (defun get-word-score (str)
 	(apply '+
-		(loop for letter across str collect (1))))
-
-
+		(loop for letter across str collect (get-letter-score letter))))
